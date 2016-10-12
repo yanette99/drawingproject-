611c871ff1ee330645520040f8a4febdbcc91759
Yanettte Abzun <yannettee.a@gmail.com>
2016-10-11 23:48:39 -0700

    finished my project

--- drawingproject.js
+++ drawingproject.js
@@ -12,6 +12,17 @@ canvas.lineTo(100,100);
 canvas.lineTo(500,10);
 canvas.fill();
 
+//window
+
+canvas.fillRect(300, 150, 100, 100);
+canvas.fillRect(550, 150, 100, 100);
+
+canvas.beginPath();
+canvas.fillStyle = "yellow";
+canvas.arc(430, 450, 10, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
 //sun
 
 canvas.beginPath();
@@ -20,8 +31,10 @@ canvas.arc(100, 80, 85, 0, 2*Math.PI);
 canvas.fill();
 canvas.stroke();
 
+//grass
+
 canvas.fillStyle = "green";
-canvas.fillRect(0, 550, 2000, 500);
+canvas.fillRect(0, 550, 2001, 500);
 
 //clouds
 
@@ -83,6 +96,44 @@ canvas.stroke();
 
 canvas.beginPath();
 canvas.fillStyle = "blue";
-canvas.arc(1050, 100, 30, 0, 2*Math.PI);
+canvas.arc(1050, 240, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(1000, 240, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(950, 240, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+//light
+
+canvas.fillStyle = "grey";
+canvas.fillRect(1040, 350, 25, 200);
+
+canvas.fillStyle = "black";
+canvas.fillRect(1025, 350, 60, 120);
+
+canvas.beginPath();
+canvas.fillStyle = "yellow";
+canvas.arc(1055, 410, 10, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "red";
+canvas.arc(1055, 380, 10, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "green";
+canvas.arc(1055, 440, 10, 0, 2*Math.PI);
 canvas.fill();
 canvas.stroke();
