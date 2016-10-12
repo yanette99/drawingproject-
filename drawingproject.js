b04744c78bcc00dbabed694a360748e6171c2bea
Yanettte Abzun <yannettee.a@gmail.com>
2016-10-11 23:21:50 -0700

    adding more shapes

--- drawingproject.js
+++ drawingproject.js
@@ -12,10 +12,77 @@ canvas.lineTo(100,100);
 canvas.lineTo(500,10);
 canvas.fill();
 
+//sun
+
 canvas.beginPath();
 canvas.fillStyle = "yellow";
-canvas.arc(100, 80, 50, 0, 2*Math.PI);
+canvas.arc(100, 80, 85, 0, 2*Math.PI);
+canvas.fill();
 canvas.stroke();
 
 canvas.fillStyle = "green";
 canvas.fillRect(0, 550, 2000, 500);
+
+//clouds
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(900, 80, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(1000, 80, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(950, 80, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(900, 120, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(1000, 120, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(950, 120, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+//2nd
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(1050, 200, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(1000, 200, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(950, 200, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
+
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(1050, 100, 30, 0, 2*Math.PI);
+canvas.fill();
+canvas.stroke();
